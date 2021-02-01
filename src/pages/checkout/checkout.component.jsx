4c55f00f8cd75790ae4103db30739f9ac2c5e65b
pranotoAdi <38913715+pranotoAdi@users.noreply.github.com>
2021-02-01 17:23:07 +0800

    added stripe.js

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { createStructuredSelector } from 'reselect';
 
 import CheckOutItem from '../../components/checkout-item/checkout-item.component';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.style.scss';
@@ -32,9 +34,13 @@ const CheckOutPage = ({cartItems, total}) => (
                 <CheckOutItem key={cartItem.id} cartItem={cartItem} />
             ))}
 
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
