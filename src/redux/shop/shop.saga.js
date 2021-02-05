b7f35823d21929002e2aef56efe2af1b90043add
pranotoAdi <38913715+pranotoAdi@users.noreply.github.com>
2021-02-05 16:36:31 +0800

    writing rooter

---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,42 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+
+import { 
+    firestore, 
+    convertCollectionSnapshotToMap 
+} from '../../firebase/firebase.utils'
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollections() {
+    try {
+        const collectionRef = firestore.collection('collections')
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call (convertCollectionSnapshotToMap, snapshot)
+        yield put (fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+     }
+    
+
+   
+    // fetch ('https://firestore.googleapis.com/v1/projects/my-shop-8d22f/databases/(default)/documents/collections')
+    // .then(response => response.json());
+    // .then(collections => console.log(collections));
+
+    // collectionRef.get().then(snapshot => {
+    //    const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+    //    dispatch(fetchCollectionsSuccess(collectionsMap));
+    // }).catch(error => dispatch(fetchCollectionsFailure(error.Message)));
+}
+
+export function* fetchCollectionsStart() {
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START, 
+        fetchCollections
+        );
+}
